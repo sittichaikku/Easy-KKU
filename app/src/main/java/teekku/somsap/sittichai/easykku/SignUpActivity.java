b3a782617da6556ba36f803701c5dd589dc8df54
teeKKU <gudjeejung@hotmail.com>
2016-11-12 16:43:06 +0700

    finish day 12 nov 59

--- app/src/main/java/teekku/somsap/sittichai/easykku/SignUpActivity.java
+++ app/src/main/java/teekku/somsap/sittichai/easykku/SignUpActivity.java
@@ -1,13 +1,24 @@
 package teekku.somsap.sittichai.easykku;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import org.jibble.simpleftp.SimpleFTP;
+
+import java.io.File;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -15,7 +26,10 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText nameEditText, phoneEditText, userEditText, passwordEditText;
     private ImageView imageView;
     private Button button;
-    private String nameString, phoneString, userString, passwordString;
+    private String nameString, phoneString, userString, passwordString,
+            imagePathString,imageNameString;
+    private Uri uri;
+    private boolean aBoolean = true;
 
 
     @Override
@@ -51,6 +65,17 @@ public class SignUpActivity extends AppCompatActivity {
                     myAlert.myDialog();
 
 
+                } else if (aBoolean) {
+                    // non choose image
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.bird48,"ยังไม่เลิอกรูป","เลือกรูปด้วย");
+                    myAlert.myDialog();;
+
+
+                } else {
+                    // chose image
+                    uploadImageToServer();
+
+
                 }
 
 
@@ -71,6 +96,29 @@ public class SignUpActivity extends AppCompatActivity {
 
     }   // main method
 
+    private void uploadImageToServer() {
+            // change Policy
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.
+                Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try{
+            SimpleFTP simpleFTP = new SimpleFTP();
+            simpleFTP.connect("ftp.swiftcodingthai.com",21,"kku@swiftcodingthai.com","Abc12345");
+            simpleFTP.bin();
+            simpleFTP.cwd("Image");
+            simpleFTP.stor(new File(imagePathString));
+            simpleFTP.disconnect();
+
+
+        }catch (Exception e){
+            Log.d("12novV1","e simpleFTP ==>" + e.toString());
+
+        }
+
+
+    } // upload
+
     @Override
     protected void onActivityResult(int requestCode,
                                     int resultCode,
@@ -79,7 +127,50 @@ public class SignUpActivity extends AppCompatActivity {
 
         if ( (requestCode == 0) && (resultCode == RESULT_OK) ) {
             Log.d("12novV1", "Result OK!!");
+            aBoolean = false;
+
+            // show image
+            uri = data.getData();
+            try{
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            // find path of image
+            imagePathString = myFindPath(uri);
+            Log.d("12novV1","imagePath ==>" + imagePathString);
+            // find name of image
+            imageNameString = imagePathString.substring(imagePathString.lastIndexOf("/"));
+            Log.d("12novV1", "imageName ==>"+imageNameString);
+
+
         } // if
 
     } // onActivity
+
+    private String myFindPath(Uri uri) {
+        String result = null;
+
+        String[] string = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri,string, null,null,null);
+        if (cursor != null){
+
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result =  cursor.getString(index);
+
+        } else {
+            result = uri.getPath();
+
+        }
+
+        return result;
+    }
+
+
 }   // main class
