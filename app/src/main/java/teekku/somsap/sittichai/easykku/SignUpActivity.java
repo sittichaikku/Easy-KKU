0f84ab182414a3f616bb63dce69fba668628eaa7
teeKKU <gudjeejung@hotmail.com>
2016-11-12 14:47:39 +0700

    alert finish

--- app/src/main/java/teekku/somsap/sittichai/easykku/SignUpActivity.java
+++ app/src/main/java/teekku/somsap/sittichai/easykku/SignUpActivity.java
@@ -2,12 +2,63 @@ package teekku.somsap.sittichai.easykku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    // explict
+    private EditText nameEditText, phoneEditText, userEditText, passwordEditText;
+    private ImageView imageView;
+    private Button button;
+    private String nameString, phoneString, userString , passwordString;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        // bind widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        phoneEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button3);
+
+        // SignUp  controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nameString = nameEditText.getText().toString().trim();
+                phoneString = phoneEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+
+                // check space
+                if (nameString.equals("")||phoneString.equals("")||
+                        userString.equals("")||passwordString.equals("")) {
+                    //have space
+                    Log.d("12novV1", "Have space");
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.bird48,"Space",
+                            "please full fill");
+                    myAlert.myDialog();
+
+
+                }
+
+
+            } // on click
+        });
+
+
+    }   // main method
+
+}   // main class
