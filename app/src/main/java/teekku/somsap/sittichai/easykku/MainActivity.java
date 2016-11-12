032e9193d4e265cbbbb9a513bcac5f659e6c604a
teeKKU <gudjeejung@hotmail.com>
2016-11-12 12:04:40 +0700

    intent to sign up finish

--- app/src/main/java/teekku/somsap/sittichai/easykku/MainActivity.java
+++ app/src/main/java/teekku/somsap/sittichai/easykku/MainActivity.java
@@ -1,13 +1,35 @@
 package teekku.somsap.sittichai.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private Button signInButton, signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // bind Widget ผูกวิเก็ดกับตัวแปร
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //sign up controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+    }   // main method
+
+}       // main class
